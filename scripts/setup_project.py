9513e487edf992653cfcd2c95d1b90fd73540e5a
najlavahdani <najlavahdani123@gmail.com>
2025-09-22 10:24:54 +0330

    chore(project): add project scaffold script to create folders and init files
    - Automatically create core directories (src, scripts, data, assets, etc.)
    - Add __init__.py files to make src, db, and utils Python packages

---
+++ scripts/setup_project.py
@@ -0,0 +1,22 @@
+import os
+
+folders = [
+    "src",
+    "scripts",
+    "data",
+    "src/db",
+    "src/utils",
+    "src/ui",
+    "assets",
+    "assets/qrcodes"
+]
+
+
+for folder in folders:
+    os.makedirs(folder, exist_ok=True)
+    
+
+# for packaging
+open("src/__init__.py", "a").close()
+open("src/db/__init__.py", "a").close()
+open("src/utils/__init__.py", "a").close()
