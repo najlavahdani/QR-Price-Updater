4829480ad012c905a2f873e9ba0fcce16900382a
najlavahdani <najlavahdani123@gmail.com>
2025-09-24 01:05:32 +0330

    Add generate_qr method to QRCodeGenerator to create and save QR code for a given product ID

--- src/services/qrcode_generator.py
+++ src/services/qrcode_generator.py
@@ -1,4 +1,5 @@
 import os
+import qrcode
 
 class QRCodeGenerator:
     def __init__(self, base_url: str):
@@ -7,3 +8,14 @@ class QRCodeGenerator:
         
         self.base_url= base_url
         os.makedirs(self.output_dir, exist_ok=True)
+        
+    def generate_qr(self, product_id: str) -> str:
+        url =f"{self.base_url}/product/{product_id}"
+        #path to save the qr code images
+        file_path= os.path.join(self.output_dir, f"{product_id}.png")
+        
+        #generating qr
+        img = qrcode.make(url)
+        img.save(file_path)
+        
+        return file_path
