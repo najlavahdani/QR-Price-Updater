36ffc31a6e42ec68eddf957a9e22db582a17574b
najlavahdani <najlavahdani123@gmail.com>
2025-09-23 22:10:50 +0330

    tests: add test for setting and retrieving exchange rate using temporary session

--- tests/test_exchange_rate.py
+++ tests/test_exchange_rate.py
@@ -1,5 +1,16 @@
 from src.db.database_manager import DatabaseManager
+from src.services.exchange_rate import ExchangeRate
+from decimal import Decimal
 
 def setup_temp_session():
     db=DatabaseManager("sqlite:///:memory:")
     return db.Session()
+
+def test_set_and_get_rate():
+    session= setup_temp_session()
+    ex_rate= ExchangeRate(session)
+    
+    ex_rate.set_rate("USD", Decimal("100000"))
+    rate= ex_rate.get_rate("USD")
+    
+    assert rate == Decimal("100000")
