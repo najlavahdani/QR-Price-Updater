0b6338919c4359c12abd54e43d9104ae011c22d2
najlavahdani <najlavahdani123@gmail.com>
2025-09-23 20:44:01 +0330

    feat: Add set_rate method to ExchangeRate class for inserting or updating currency exchange rates

--- src/services/ExchangeRate.py
+++ src/services/ExchangeRate.py
@@ -1,5 +1,18 @@
 from sqlalchemy.orm import Session
-
+from decimal import Decimal
+from src.db.models import Settings
 class ExchangeRate:
     def __init__(self, session: Session):
         self.session = session
+        
+    
+    def set_rate(self, currency: str, rate: Decimal):
+        #add or update if already exists
+        setting= self.session.query(Settings).filter_by(currency=currency).first()
+        if setting: #true if the currency already exists
+            setting.exchange_rate = rate
+        else:
+            setting= Settings(currency=currency, exchange_rate=rate)
+            self.session.add(setting)
+        self.session.commit()
+        return setting #return the updated or new exchange setting
