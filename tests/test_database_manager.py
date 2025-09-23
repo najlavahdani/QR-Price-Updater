1b92415fdffd710a4ece6e5955feb4dcc2605b15
najlavahdani <najlavahdani123@gmail.com>
2025-09-23 12:28:01 +0330

    tests: add unit test for product insertion in temporary in-memory DB

--- tests/test_database_manager.py
+++ tests/test_database_manager.py
@@ -5,3 +5,19 @@ def setup_temp_db():
     #creating temporary database 
     db = DatabaseManager("sqlite:///:memory:")
     return db
+
+def test_insert_update_product():
+    db = setup_temp_db()
+    
+    products = [
+        {"ProductID": "P100", "Name": "Laptop Dell", "PriceUSD": "1000.50"},
+        {"ProductID": "P101", "Name": "Laptop HP", "PriceUSD": "950.75"},
+    ]
+    
+    #insertion test
+    insertion_result= db.insert_or_update_products(products)
+    assert insertion_result[0]["action"] == "inserted"
+    assert insertion_result[1]["action"] == "inserted"
+    
+
+
