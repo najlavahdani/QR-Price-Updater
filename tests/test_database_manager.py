ff531a0565fe6d1eb7b96b1c035cfb8e9c9f72d8
najlavahdani <najlavahdani123@gmail.com>
2025-09-24 11:57:11 +0330

    Separate tests for DatabaseManager and QRCodeGenerator into different files

--- tests/test_database_manager.py
+++ tests/test_database_manager.py
@@ -2,10 +2,6 @@ from src.db.database_manager import DatabaseManager
 from decimal import Decimal
 import os
 
-BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-QR_TEST_DIR= os.path.join(os.path.join(os.path.join(BASE_DIR, "assets"), "qrcodes"), "test")
-os.makedirs(QR_TEST_DIR, exist_ok=True)
-
 def setup_temp_db(tmp_path=None):
     #creating temporary database 
     db = DatabaseManager("sqlite:///:memory:")
