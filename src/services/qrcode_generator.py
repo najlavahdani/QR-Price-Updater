0a11d969580f554d3a3d1215a4d7d135ecd1fec4
najlavahdani <najlavahdani123@gmail.com>
2025-09-24 12:49:51 +0330

    Add support for custom QR code output directory in QRCodeGenerator; use provided png_files_dir if given, otherwise fallback to default path.

--- src/services/qrcode_generator.py
+++ src/services/qrcode_generator.py
@@ -2,11 +2,17 @@ import os
 import qrcode
 
 class QRCodeGenerator:
-    def __init__(self, base_url: str):
-        root_path= os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
-        self.output_dir= os.path.join(os.path.join(root_path, "assets"), "qrcodes")
-        
+    def __init__(self, base_url: str, png_files_dir: str= None): 
         self.base_url= base_url
+        
+        if png_files_dir:
+            #costum path
+            self.output_dir= png_files_dir
+        else:
+            #default path
+            root_path= os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+            self.output_dir= os.path.join(os.path.join(root_path, "assets"), "qrcodes")
+        
         os.makedirs(self.output_dir, exist_ok=True)
         
     def generate_qr(self, product_id: str) -> str:
