e5420c43618ca176a36b74d795aeaa1e36be5c49
najlavahdani <najlavahdani123@gmail.com>
2025-09-25 00:34:06 +0330

    setup: initialize FastAPI app and database session dependency
    - Create FastAPI app instance
    - Initialize DatabaseManager with SQLite database at PROJECT_ROOT/data/app.db
    - Define get_session dependency to provide and automatically close DB sessions

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,19 @@
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
-
+from fastapi import FastAPI, Request
+from src.db.database_manager import DatabaseManager
+from src.services.exchange_rate import ExchangeRate
 #project and template path
 PROJECT_ROOT=  Path(__file__).resolve().parents[2]
 templates= Jinja2Templates(directory=str(Path(__file__).resolve().parent / "templates"))
 
+app = FastAPI()
+
+db= DatabaseManager(f"sqlite:///{PROJECT_ROOT}/data/app.db")
+
+#dependency for Session
+def get_session():
+    with db.Session() as session:
+        yield session
+        
+ 
