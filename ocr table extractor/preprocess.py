bc5fbc39042159f60e4ae2a42f7e8d335a53ece8
najlavahdani <najlavahdani123@gmail.com>
2025-09-27 01:25:19 +0330

    Implement to_grayscale function using OpenCV to improve OCR performance

--- ocr table extractor/preprocess.py
+++ ocr table extractor/preprocess.py
@@ -5,3 +5,7 @@ def read_image(path):
     if img is None: #invalid oath
         raise FileNotFoundError(f"Image not found: {path}")
     return img 
+
+#Convert to grayscale color space for faster and more accurate OCR
+def to_grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
