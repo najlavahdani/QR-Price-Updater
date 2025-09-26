353f23863ab2f84a7a5b0d83e041b7497be723f3
najlavahdani <najlavahdani123@gmail.com>
2025-09-27 00:46:30 +0330

    feat(endpoints): add /product/{product_id} route to display product details with local price
    - Created GET endpoint `/product/{product_id}` to fetch product info from DB
    - Integrated ExchangeRate service to calculate price in local currency
    - Returns 404 if product not found

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException
 from src.db.database_manager import DatabaseManager
 from src.services.exchange_rate import ExchangeRate
+from decimal import Decimal
 #project and template path
 PROJECT_ROOT=  Path(__file__).resolve().parents[2]
 templates= Jinja2Templates(directory=str(Path(__file__).resolve().parent / "templates"))
@@ -13,7 +14,22 @@ db= DatabaseManager(f"sqlite:///{PROJECT_ROOT}/data/app.db")
 
 #dependency for Session
 def get_session():
+    #create a session and automatically release it after work is done
     with db.Session() as session:
         yield session
-        
- 
+
+
+#endpoints
+@app.get(f"/product/{"product_id"}")
+async def product_page(request: Request, product_id: str, session=Depends(get_session)):
+    #Exchange instance
+    exchange = ExchangeRate(session) 
+    
+    #retrieve product from DB
+    product= db.get_product_by_id(product_id)   
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    #calculate local price
+    original_price= Decimal(str(product.price))
+    local_price= exchange.calculate_price(original_price,)
