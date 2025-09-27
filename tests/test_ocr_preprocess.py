3ced69a17cabe3966080f41e50ffb525e078271f
najlavahdani <najlavahdani123@gmail.com>
2025-09-27 10:42:41 +0330

    Use os.makedirs instead of os.mkdir for recursive directory creation (supported in newer Python versions)

--- tests/test_ocr_preprocess.py
+++ tests/test_ocr_preprocess.py
@@ -6,7 +6,7 @@ import cv2
 
 @pytest.fixture
 def tmp_image_path():
-    os.mkdir("tests/tests_outputs/output_images", exist_ok=True)
+    os.makedirs("tests/tests_outputs/output_images", exist_ok=True)
     path = "tests/output_images/white_img.jpg"
     img = np.full((50, 50, 3), 255, dtype=np.uint8)
     cv2.imwrite(path, img)
