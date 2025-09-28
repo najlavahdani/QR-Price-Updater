d482661838313683bc3cf6bea71a0f673ca850ac
najlavahdani <najlavahdani123@gmail.com>
2025-09-28 17:51:37 +0330

    Create temp_session fixture for tests with in-memory SQLite DB, closing session after use

--- tests/test_db_setup.py
+++ tests/test_db_setup.py
@@ -1,9 +1,18 @@
 import pytest
 from sqlalchemy import create_engine
 from src.db.database import Base
+from sqlalchemy.orm import sessionmaker
+
 @pytest.fixture
 def temp_engine():
     engine=create_engine("sqlite:///:memory:", echo=False, future=True)
     Base.metadata.create_all(engine)
     yield engine
     engine.dispose()
+    
+@pytest.fixture
+def temp_session(temp_engine):
+    Session = sessionmaker(bind=temp_engine, autoflush=False, future=True)
+    session= Session()
+    yield session
+    session.close()
