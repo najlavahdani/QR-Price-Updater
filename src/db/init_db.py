b8a2e4c7696b7567859f6db244cf976a90e17565
najlavahdani <najlavahdani123@gmail.com>
2025-09-28 12:24:47 +0330

    db: add database seeding for default Settings row
    - init_db() ensures tables are created if they don't exist
    - seed_settings() adds a default Settings row with exchange_rate=-1 if table is empty
    - Prevents overwriting existing data and provides initial default configuration

---
+++ src/db/init_db.py
@@ -0,0 +1,32 @@
+from src.db.database import get_session, init_db
+from src.db.models import Settings
+from decimal import Decimal
+
+def seed_settings():
+    """
+        it only adds a default row that exchange_rate=-1 in the settings table if the table is empty,
+        after that, no changes to exchange_rate will be applied
+    """
+    with get_session() as session:
+        exists = session.query(Settings).first()
+        if not exists:
+            default_setting = Settings(
+                id=1,
+                currency = "USD",
+                exchange_rate= Decimal("-1")                
+            )
+            session.add(default_setting)
+            session.commit()
+            print("Settings table seeded with default row.")
+        else:
+            print("Settings table already has data. Skipping seeding.")
+
+def main():
+    #creating tables if if they don's exist
+    init_db()
+    
+    #add default settings row only if Settings table is empty
+    seed_settings()
+    
+if __name__ == "__main__":
+    main()
