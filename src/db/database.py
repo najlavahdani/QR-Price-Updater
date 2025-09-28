6be39912663674f20acb873ac75f7f61d0ca2ca6
najlavahdani <najlavahdani123@gmail.com>
2025-09-28 11:42:24 +0330

    db: add get_engine and get_session dependency
    - get_engine() returns the SQLAlchemy engine instance
    - get_session() provides a generator-based Session for FastAPI or other usage
    - Ensures sessions are properly closed after use with context manager

--- src/db/database.py
+++ src/db/database.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session as SessionType
-
+from typing import Generator
 #paths
 BASE_DIR= Path(__file__).resolve().parents[1] #src/
 DATA_DIR= BASE_DIR/"data"
@@ -25,3 +25,14 @@ def init_db() -> None:
     """
     import src.db.models
     Base.metadata.create_all(bind=engine)
+    
+def get_engine():
+    return engine
+
+def get_session() -> Generator[SessionType, None, None]:
+    """
+        Dependency for FastAPI or anywhere else.
+        Use in route: session: Session = Depends(get_session)
+    """
+    with SessionLocal() as session:
+        yield session
