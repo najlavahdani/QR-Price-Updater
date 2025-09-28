7b84b17fd5176f9879d320de67f2bad5bb9665f2
najlavahdani <najlavahdani123@gmail.com>
2025-09-29 00:19:26 +0330

    Refactor ExchangeRate tests to use in-memory temporary database and remove unnecessary Base import

--- tests/test_exchange_rate.py
+++ tests/test_exchange_rate.py
@@ -1,38 +1,46 @@
-from src.db.database_manager import DatabaseManager
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.db.database import init_db
 from src.services.exchange_rate import ExchangeRate
 from decimal import Decimal
 
-def setup_temp_session():
-    db=DatabaseManager("sqlite:///:memory:")
-    return db.Session()
-
-
-def test_set_and_get_rate():
-    session= setup_temp_session()
-    ex_rate= ExchangeRate(session)
+#----------fixtures----------
+@pytest.fixture
+def temp_session():
+    #Creating a session on a temporary database in memory
+    engine = create_engine("sqlite:///:memory:", future=True)
+    init_db(engine_arg=engine)
+    SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False, future=True)
+    session = SessionLocal()
+    yield session
+    session.close()
+    engine.dispose()
     
-    ex_rate.set_rate("USD", Decimal("100000"))
-    rate= ex_rate.get_rate("USD")
+#----------text----------
+def test_set_and_get_rate(temp_session):
+    ex_rate= ExchangeRate(temp_session)
+    
+    ex_rate.set_rate(Decimal("100000"))
+    rate= ex_rate.get_rate()
     
     assert rate == Decimal("100000")
 
     
-def test_update_rate():
-    session= setup_temp_session()
-    ex_rate= ExchangeRate(session)
+def test_update_rate(temp_session):
+    ex_rate= ExchangeRate(temp_session)
     
-    ex_rate.set_rate("USD", Decimal("60000"))
-    ex_rate.set_rate("USD", Decimal("65000"))  #update
+    ex_rate.set_rate(Decimal("60000"))
+    ex_rate.set_rate(Decimal("65000"))  #update
     
-    rate= ex_rate.get_rate("USD")
+    rate= ex_rate.get_rate()
     assert rate == Decimal("65000")
     
 
-def test_calculate_price():
-    session = setup_temp_session()
-    ex_rate = ExchangeRate(session)
+def test_calculate_price(temp_session):
+    ex_rate = ExchangeRate(temp_session)
     
-    ex_rate.set_rate("USD", Decimal("60000"))
-    result= ex_rate.calculate_price(Decimal("2"), "USD")
+    ex_rate.set_rate(Decimal("60000"))
+    result= ex_rate.calculate_price(Decimal("2"))
     
     assert Decimal(120000) == result
