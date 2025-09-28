b1ea26122e2bf2569f7ecab1bf613a473f9b2a9e
najlavahdani <najlavahdani123@gmail.com>
2025-09-28 18:01:10 +0330

    Test get_session fixture with a simple SELECT 1 query to ensure session and DB connection are valid

--- tests/test_db_setup.py
+++ tests/test_db_setup.py
@@ -2,6 +2,8 @@ import pytest
 from sqlalchemy import create_engine
 from src.db.database import Base
 from sqlalchemy.orm import sessionmaker
+from src.db.database import get_session
+from sqlalchemy import inspect
 
 @pytest.fixture
 def temp_engine():
@@ -16,3 +18,10 @@ def temp_session(temp_engine):
     session= Session()
     yield session
     session.close()
+    
+def test_get_session(temp_session): #creating in-memory temp db and session
+    with get_session() as session:
+        #this command does not require any tables, it is only used to test the connection and correct db performance
+        result = session.Execute("SELECT 1").scaler()
+        assert result == 1
+    
