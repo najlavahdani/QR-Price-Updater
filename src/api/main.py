1929adf3a32a9cdd3a25e9ae3d056fe530d97922
najlavahdani <najlavahdani123@gmail.com>
2025-09-30 08:14:45 +0330

    feat: add FastAPI endpoint to display product page with local price
    - Set up FastAPI app with Jinja2 templates
    - Create /product/{product_id} GET endpoint
    - Retrieve product from DatabaseManager
    - Calculate local price using ExchangeRate service
    - Render HTML template with product details and local price
    - Handle missing product and missing exchange rate cases

---
+++ src/api/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Request, Depends, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from pathlib import Path
+from sqlalchemy.orm import Session
+from decimal import Decimal
+
+from src.db.database import get_session
+from src.db.database_manager import DatabaseManager
+from src.services.exchange_rate import ExchangeRate
+
+#Create a DatabaseManager instance with local base_url
+db = DatabaseManager(qr_base_url="http://127.0.0.1:8000")
+
+#templates path
+BASE_DIR=Path(__file__).resolve().parent #C:\Users\Najla\Desktop\QR price updater\src\api
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+#fastapi application
+app= FastAPI(title="Product QR Service")
+
+@app.get("/product/{product_id}", response_class=HTMLResponse)
+def product_page(
+    request: Request,
+    product_id: str,
+    session: Session = Depends(get_session)
+):
+    #retrieving product
+    product= db.get_product_by_id(product_id= product_id, session=session)
+    if not product:
+        raise HTTPException(status_code=404, detail=" Product not found")
+    
+    #price calculating
+    ex_rate= ExchangeRate(session=session)
+    try:
+        local_price = ex_rate.calculate_price(Decimal(str(usd_price= product.price)))
+        local_price_str=f"{local_price} تومان"
+    except Exception as e:
+        local_price_str= "نرخ ارز تعریف نشده است"
+        
+    #render HTML template
+    return templates.TemplateResponse(
+        "product.html",
+        {
+            "request": request,
+            "product": product,
+            "local_price": local_price_str,
+        }
+    )
