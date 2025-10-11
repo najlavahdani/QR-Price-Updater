df381547c8a8e43c46b7758abda304b9ac7093c8
najlavahdani <najlavahdani123@gmail.com>
2025-10-11 20:53:54 +0330

    feat(api): add Pydantic schemas for product

--- src/api/main.py
+++ src/api/main.py
@@ -4,14 +4,15 @@ from fastapi.responses import HTMLResponse
 from pathlib import Path
 from sqlalchemy.orm import Session
 from decimal import Decimal
-from typing import Annotated, Any
-from src.db.database import get_session
+from pydantic import BaseModel
 
+from src.db.database import get_session
 from src.db.database_manager import DatabaseManager
 from src.services.exchange_rate import ExchangeRate
 
+
 #Create a DatabaseManager instance with local base_url
-db = DatabaseManager(qr_base_url="http://127.0.0.1:8000")
+db_manager = DatabaseManager(qr_base_url="http://127.0.0.1:8000")
 
 #templates path
 BASE_DIR=Path(__file__).resolve().parent #C:\Users\Najla\Desktop\QR price updater\src\api
@@ -20,6 +21,7 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 #fastapi application
 app= FastAPI(title="Product QR Service")
 
+#html page rout (QR code)
 @app.get("/product/{product_id}", response_class=HTMLResponse)
 def product_page(
     request: Request,
@@ -32,17 +34,11 @@ def product_page(
         session = next(session)
 
     #retrieving product
-    product= db.get_product_by_id(product_id= product_id, session=session)
+    product= db_manager.get_product_by_id(product_id= product_id, session=session)
     if not product:
         raise HTTPException(status_code=404, detail=" Product not found")
     
-    #price calculating
-    # ex_rate= ExchangeRate(session=session)
-    # if ex_rate.get_rate() == Decimal(-1):
-    #     local_price_str= "نرخ ارز تعریف نشده است"
-    # else:
-    #     local_price=ex_rate.calculate_price(product.price)
-    #     local_price_str= f"{local_price} تومان"
+    
     ex_rate = ExchangeRate()  # pass the real session here
     try:
         local_price_str = f"{ex_rate.calculate_price(product.price)} تومان"
@@ -50,7 +46,6 @@ def product_page(
         local_price_str = "نرخ ارز تعریف نشده است"   
         
     #render HTML template
-    
     product_dict = {
         "ProductID": product.product_id,
         "Name": product.name, 
@@ -64,3 +59,19 @@ def product_page(
             "local_price": local_price_str,
         }
     )
+
+
+#json API endpoints for progarm
+#pydantic schemas
+class ProductCreate(BaseModel):
+    ProductID: str
+    Name: str
+    PriceUSD: Decimal
+    
+class ProductRead(BaseModel):
+    ProductID: str
+    Name: str
+    PriceUSD: Decimal
+    
+    class config:
+        orm_mode=True
